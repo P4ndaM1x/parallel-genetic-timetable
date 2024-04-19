dc8f2732a0803373f7df88f24c187510d458b83a
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2024-04-19 19:15:37 +0200

    Add .clang-format file and update CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-// MPI program for calculating Pi by numerical integration - basic code framework 
+// MPI program for calculating Pi by numerical integration - basic code framework
 
 #include "mpi.h"
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int n = 0, myid = 0, numprocs = 1, i;
     double PI25DT = 3.141592653589793238462643;
@@ -19,10 +19,8 @@ int main(int argc, char *argv[])
      * the function curve in every loop iteration
      */
 
-    while (true)
-    {
-        if (myid == 0)
-        {
+    while (true) {
+        if (myid == 0) {
             printf("Enter the number of intervals: (0 quits) ");
             scanf("%d", &n);
         }
@@ -32,11 +30,9 @@ int main(int argc, char *argv[])
 
         h = 1.0 / (double)n;
         sum = 0.0;
-        for (i = myid + 1; i <= n; i += numprocs)
-        {
+        for (i = myid + 1; i <= n; i += numprocs) {
             x = h * ((double)i - 0.5);
-            sum += 4.0 / (1.0 +x*x);
-
+            sum += 4.0 / (1.0 + x * x);
         }
         mypi = h * sum;
 
@@ -45,8 +41,7 @@ int main(int argc, char *argv[])
 
         if (myid == 0)
             printf("pi is approximately %.16f, Error is %.16f\n",
-                   pi, fabs(pi - PI25DT));
-
+                pi, fabs(pi - PI25DT));
     }
 
     MPI_Finalize();
