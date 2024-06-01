3a1247e84771f947ae6f2fff65790c4781e17aa3
P4ndaM1x <77618102+P4ndaM1x@users.noreply.github.com>
2024-06-01 22:28:50 +0200

    Refactor main.cpp for UPCXX support and improve code organization

--- main.cpp
+++ main.cpp
@@ -12,24 +12,22 @@
 #include "MPINode.hpp"
 #else
 #include "UPCNode.hpp"
-#include <upcxx-extras/dist_array/dist_array.hpp>
 #endif
 
 int main(int argc, char* argv[])
 {
 #ifdef UPCXX
-
     UPCNode node{};
+#else
+    MPINode node{&argc, &argv};
+#endif
+
     CLI::Args::prepare(argc, argv, node);
     srand(time(NULL) + node.getRank());
 
-    const auto numberOfWorkers = node.getSize();
+    const auto numberOfWorkers = node.getSize() - 1; // Master node is not a worker
     const auto populationSizePerWorker = CLI::Args::populationSize / numberOfWorkers
         + (CLI::Args::populationSize % numberOfWorkers != 0); // ceil
-
-    upcxx::extras::dist_array<std::optional<Chromosome>> distributedArray{
-        numberOfWorkers * populationSizePerWorker, populationSizePerWorker
-    };
 
     if (node.isMaster()) {
         Timetable localTimetable;
@@ -38,26 +36,6 @@ int main(int argc, char* argv[])
     }
     // Broadcast timetable to worker nodes
     node.broadcastMessageFromMaster();
-
-    // if (node.getRank() == 1) {
-    //     Timetable{Timetable::deserializeClasses(node.getMessage())}.printClasses();
-    //     node.setMessage("cokolwiek");
-    //     node.sendMessageToMaster();
-    // }
-    // upcxx::barrier();
-
-    // if (node.isMaster()) {
-    //     // node.receiveMessageFromWorker(1);
-    //     // Log::print(node.getMessage());
-    //     std::cout << node.getMessage() << std::endl;
-    // }
-
-    // upcxx::barrier();
-    // if (node.getRank() == 2) {
-    //     // node.receiveMessageFromWorker(1);
-    //     // Log::print(node.getMessage());
-    //     std::cout << node.getMessage() << std::endl;
-    // }
 
     GeneticAlgorithm geneticAlgorithm{
         Timetable::deserializeClasses(node.getMessage()),
@@ -97,11 +75,14 @@ int main(int argc, char* argv[])
 
         if (not node.isMaster()) {
             geneticAlgorithm.run();
-            // Send best population sample to master node
             node.setMessage(geneticAlgorithm.serializePopulationOfSize(populationSizePerWorker));
-            // node.sendMessageToMaster();
+#ifndef UPCXX
+            // Send best population sample to master node
+            node.sendMessageToMaster();
+#endif
         }
 
+#ifdef UPCXX
         GeneticAlgorithm::ChromosomeContainer gatheredChromosomes;
         for (int workerRank = 1; workerRank < node.getSize(); ++workerRank) {
 
@@ -120,81 +101,10 @@ int main(int argc, char* argv[])
             }
             upcxx::barrier();
         }
-
         if (node.isMaster()) {
             node.setMessage(GeneticAlgorithm::serializePopulation(gatheredChromosomes));
         }
-
-        // Broadcast gathered chromosomes to all nodes
-        node.broadcastMessageFromMaster();
-    }
-
-    if (node.isMaster()) {
-        geneticAlgorithm.getPopulation().at(0).printSolution();
-    }
-    Log::print("All done!\n");
-
 #else
-
-    MPINode node{&argc, &argv};
-    CLI::Args::prepare(argc, argv, node);
-    srand(time(NULL) + node.getRank());
-
-    const auto numberOfWorkers = node.getSize() - 1; // Master node is not a worker
-    const auto populationSizePerWorker = CLI::Args::populationSize / numberOfWorkers
-        + (CLI::Args::populationSize % numberOfWorkers != 0); // ceil
-
-    if (node.isMaster()) {
-        Timetable timetable;
-        FileManager::loadClasses(CLI::Args::dataFilePath, timetable);
-        node.setMessage(Timetable::serializeClasses(timetable.getClasses()));
-    }
-    // Broadcast timetable to worker nodes
-    node.broadcastMessageFromMaster();
-
-    GeneticAlgorithm geneticAlgorithm{
-        Timetable::deserializeClasses(node.getMessage()),
-        CLI::Args::populationSize,
-        CLI::Args::numberOfGenerations,
-        CLI::Args::mutationRate
-    };
-
-    const auto gatheringSteps = CLI::Args::numberOfGenerations / CLI::Args::gatheringFrequency;
-    for (unsigned step = 0; step <= gatheringSteps; ++step) {
-        if (step != 0) {
-            constexpr unsigned maxChromosomesToPrint = 3u;
-
-            GeneticAlgorithm::ChromosomeContainer updatedPopulation
-                = GeneticAlgorithm::deserializePopulation(node.getMessage());
-            geneticAlgorithm.setPopulation(updatedPopulation);
-            GeneticAlgorithm::ChromosomeContainer bestPopulation
-                = geneticAlgorithm.getBestPopulationOfSize(maxChromosomesToPrint);
-
-            if (node.isMaster()) {
-                Log::print(
-                    "Best chromosomes after gathering step " + std::to_string(step) + '/'
-                    + std::to_string(gatheringSteps) + ":"
-                );
-                for (unsigned i = 0; i < bestPopulation.size(); i++) {
-                    Log::print(
-                        std::to_string(i + 1u)
-                        + ". best chromosome error: " + std::to_string(bestPopulation[i].getError())
-                    );
-                }
-            }
-            if (bestPopulation.at(0).getError() <= 0)
-                break;
-            if (step == gatheringSteps)
-                break;
-        }
-
-        if (not node.isMaster()) {
-            geneticAlgorithm.run();
-            // Send best population sample to master node
-            node.setMessage(geneticAlgorithm.serializePopulationOfSize(populationSizePerWorker));
-            node.sendMessageToMaster();
-        }
-
         if (node.isMaster()) {
             GeneticAlgorithm::ChromosomeContainer gatheredChromosomes;
             for (int workerRank = 1; workerRank < node.getSize(); ++workerRank) {
@@ -210,6 +120,7 @@ int main(int argc, char* argv[])
             }
             node.setMessage(GeneticAlgorithm::serializePopulation(gatheredChromosomes));
         }
+#endif
 
         // Broadcast gathered chromosomes to all nodes
         node.broadcastMessageFromMaster();
@@ -219,7 +130,6 @@ int main(int argc, char* argv[])
         geneticAlgorithm.getPopulation().at(0).printSolution();
     }
     Log::print("All done!\n");
-#endif
 
     return 0;
 }
